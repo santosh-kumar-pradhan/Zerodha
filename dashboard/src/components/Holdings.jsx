d1ea68fdd8cc4a446bde214d2abf364e1bc5376c
Santosh Kumar Pradhan <147923912+santosh-kumar-pradhan@users.noreply.github.com>
2025-04-05 17:50:48 +0530

    Update BASE URL

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -8,7 +8,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3005/allHoldings").then((res) => {
+    axios.get("https://zerodha-6cbc.onrender.com/allHoldings").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
